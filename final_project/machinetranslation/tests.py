2ad80eb3b3bb9ac0d7b23c84a223a2ae30d4ff38
Bhanu-Priya6 <139629831+Bhanu-Priya6@users.noreply.github.com>
2023-07-26 16:04:23 +0530

    Update tests.py

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,14 +1,23 @@
+""" Unit tests """
 import unittest
+from translator import english_to_french, french_to_english
 
-from translator import englishToFrench, frenchToEnglish
+class TestEnglishToFrench(unittest.TestCase):
+    """ English to French """
+    def test1(self):
+        self.assertIsNotNone(english_to_french('Hello'))
+    def test2(self):
+        self.assertEqual(english_to_french('Hello'),'Bonjour')
+    def test3(self):
+        self.assertNotEqual(english_to_french('Hello'),'Hallo')
 
-class TestenglishToFrench(unittest.TestCase): 
-    def test1(self): 
-        self.assertEqual(englishToFrench("Hello"), "Bonjour") # test when Hello is given as input the output is Bonjour.
-        self.assertEqual(englishToFrench("Bye"), "Au revoir") # test when Bye is given as input the output is Au revoir.
-       
-class TestfrenchToEnglish(unittest.TestCase): 
-    def test1(self): 
-        self.assertEqual(frenchToEnglish("Bonjour"), "Hello") # test when Bonjour is given as input the output is Hello.
-        self.assertEqual(frenchToEnglish("Au revoir"), "Bye") # test when Au revoir is given as input the output is Bye.
+class TestFrenchToEnglish(unittest.TestCase):
+    """ French to English """
+    def test1(self):
+        self.assertIsNotNone(french_to_english('Bonjour'))
+    def test2(self):
+        self.assertEqual(french_to_english('Bonjour'),'Hello')
+    def test3(self):
+        self.assertNotEqual(french_to_english('Bonjour'),'Guten Tag')
+
 unittest.main()
